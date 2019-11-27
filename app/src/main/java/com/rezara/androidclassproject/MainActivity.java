1631091b9a61a45f63d90437e6fc06bfb7672d9c
Rza Ra <lotus@Rza-Ra.local>
2019-11-27 13:39:52 +0330

    adding activity lifecycle

--- app/src/main/java/com/rezara/androidclassproject/MainActivity.java
+++ app/src/main/java/com/rezara/androidclassproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.rezara.androidclassproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Mytag","onCreate");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Mytag","onStart");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    Log.d("Mytag","onResume");
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+    Log.d("Mytag","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    Log.d("Mytag","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    Log.d("Mytag","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    Log.d("Mytag","onRestart");
+    }
 }
